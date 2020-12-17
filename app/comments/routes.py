33efb6e737754b1647553f1b55fc235c97e0086e
lizaost <lizaost@gmail.com>
2020-12-17 16:51:29 +0600

    Added API methods for getting and creating comments.

--- app/comments/routes.py
+++ app/comments/routes.py
@@ -1,16 +1,36 @@
-from app import app
+from app import app, db
+from app.comments.model import Comment
+from flask import jsonify, request
+from datetime import datetime
 
 
-@app.route('/comments/<id>', methods=['GET'])
-def get_comment(id):
-    return "Getting a comment with id " + str(id)
+@app.route('/comments/<comment_id>', methods=['GET'])
+def get_comment(comment_id):
+    comment = Comment.query.filter(Comment.id == comment_id).first()
+    if comment:
+        comment_dict = comment.to_dict()
+        return jsonify(comment_dict)
+    else:
+        return jsonify({})
 
 
-@app.route('/posts/<id>/comments', methods=['GET'])
-def get_post_comments(id):
-    return "Getting comments for post with id " + str(id)
+@app.route('/posts/<post_id>/comments', methods=['GET'])
+def get_post_comments(post_id):
+    comments = Comment.query.filter(Comment.post_id == post_id)
+    if comments:
+        comments_list = [comment.to_dict() for comment in comments]
+        return jsonify(comments_list)
+    else:
+        return jsonify([])
 
 
 @app.route('/comments', methods=['POST'])
 def create_comment():
-    return "Creating a new comment"
+    author_id = 40  # TODO: get user_id from token
+    text = request.form.get('text')
+    post_id = request.form.get('post_id')
+    published_at = datetime.now()
+    comment = Comment(text=text, author_id=author_id, post_id=post_id, published_at=published_at)
+    db.session.add(comment)
+    db.session.commit()
+    return 'ok'
